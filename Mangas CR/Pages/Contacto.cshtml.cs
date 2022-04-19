30ae8dbe39821fb43bdfaeb082d41aa8d53eb6e4
yerlin12 <97709544+yerlin12@users.noreply.github.com>
2022-04-19 15:35:16 -0600

    Agregar archivos de proyecto.

---
+++ Mangas CR/Pages/Contacto.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mangas_CR.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
